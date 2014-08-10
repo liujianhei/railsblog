06e05c40db5993d3350bf70761f1e35646c77832
Jianhong Liu <jianheiliu@gmail.com>
2014-08-10 22:54:49 +0800

    tag

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140806154258) do
+ActiveRecord::Schema.define(version: 20140810144116) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -35,5 +35,18 @@ ActiveRecord::Schema.define(version: 20140806154258) do
 
   add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable"
   add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type"
+
+  create_table "taggings", force: true do |t|
+    t.integer  "article_id"
+    t.integer  "tag_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
